d139db6bd6479c780c9b6556817b958e5d204fc6
Suratin Pattanawongthai <suratin.p@gobike.asia>
2019-06-01 08:28:35 +0700

    WIP: implement reconstitution method in game factory

--- src/Game/GameFactory.js
+++ src/Game/GameFactory.js
@@ -1,18 +1,26 @@
+const _ = require('lodash');
 const { Game } = require('.');
 
 class GameFactory {
 
     /**
+     * @param {string} id
      * @param {number} weight
      * @param {number} height
      * @returns {Game}
      */
-    static create(weight, height) {
-        return new Game(weight, height);
+    create(id, weight, height) {
+        return new Game(id, weight, height);
     }
 
-    static reconstitute() {
-
+    /**
+     * @returns {Game}
+     */
+    reconstitute(gameEntityModel) {
+        const prototype = new Game('1', 10, 10);
+        gameEntityModel = _.pick(gameEntityModel, Object.keys(prototype))
+        const game = Object.assign(new Game('1', 10, 10), gameEntityModel);
+        return game;
     }
 }
 module.exports = GameFactory;
