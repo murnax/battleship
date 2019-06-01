cfebf4b0193ff82df0771cc4648333f47a413675
Suratin Pattanawongthai <suratin.p@gobike.asia>
2019-06-01 08:29:26 +0700

    call place ship on game model

--- src/Game/GameService.js
+++ src/Game/GameService.js
@@ -1,18 +1,32 @@
-const { GameRepository, GameFactory, Game } = require('.');
+const uuid = require('uuid');
+const { GameRepository, GameFactory, Game, Ship, Coordinate } = require('.');
 
 class GameService {
 
     /**
      * 
      * @param {GameRepository} gameRepository 
+     * @param {GameFactory} gameFactory
      */
-    constructor(gameRepository) {
+    constructor(gameRepository, gameFactory) {
         this._gameRepository = gameRepository;
+        this._gameFactory = gameFactory;
     }
 
     async startGame() {
-        const game = GameFactory.create(10, 10);
+        const game = this._gameFactory.create(uuid(), 10, 10);
         await this._gameRepository.create(game);
+    }
+
+    /**
+     * 
+     */
+    async placeShip(id, shipType, x, y, direction) {
+        const ship = Ship.Ship.create(uuid(), shipType);
+        const game = await this._gameRepository.getGameByID(id);
+
+        game.placeShip(ship, new Coordinate(x, y), direction);
+        await this._gameRepository.update(game);
     }
 }
 module.exports = GameService;
