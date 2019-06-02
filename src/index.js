c34810326dcbc107403499eb8f0bf2a19febedda
Suratin Pattanawongthai <suratin.p@gobike.asia>
2019-06-02 20:58:20 +0700

    add exception handling

--- src/index.js
+++ src/index.js
@@ -14,50 +14,68 @@ const { GameService, GameRepository, GameFactory } = require('./Game');
 const gameFactory = new GameFactory();
 const gameService = new GameService(new GameRepository(null, gameFactory), gameFactory);
 
-app.get('/', async (req, res) => {
-    const { id, user_type } = req.query;
-    const board = await gameService.getBoard(id, user_type.toUpperCase());
-    res.json({
-        status: 200,
-        data: {
-            board
-        }
-    });
+app.get('/', async (req, res, next) => {
+    try {
+        const { id, user_type } = req.query;
+        const board = await gameService.getBoard(id, user_type.toUpperCase());
+        res.json({
+            status: 200,
+            data: {
+                board
+            }
+        });
+    } catch (error) {
+        next(error);
+    }
 });
 
-app.post('/', async (req, res) => {
-    const game = await gameService.startGame();
-    res.json({
-        status: 200,
-        data: {
-            id: game.id
-        }
-    });
+app.post('/', async (req, res, next) => {
+    try {
+        const game = await gameService.startGame();
+        res.json({
+            status: 200,
+            data: {
+                id: game.id
+            }
+        });
+    } catch (error) {
+        next(error);
+    }
 });
 
-app.post('/reset', async (req, res) => {
-    await gameService.reset(req.body.id);
-    res.json({
-        status: 200
-    });
+app.post('/reset', async (req, res, next) => {
+    try {
+        await gameService.reset(req.body.id);
+        res.json({
+            status: 200
+        });
+    } catch (error) {
+        next(error);
+    }
 });
 
-app.post('/attack', async (req, res) => {
-    const { id, x, y } = req.body;
-    await gameService.attack(id, x, y);
-    res.json('ok');
+app.post('/attack', async (req, res, next) => {
+    try {
+        const { id, x, y } = req.body;
+        await gameService.attack(id, x, y);
+        res.json('ok');
+    } catch (error) {
+        next(error);
+    }
 });
 
-app.post('/ship', async (req, res) => {
-    const { id, direction, x, y, ship_type } = req.body;
-    await gameService.placeShip(id, ship_type, x, y, direction);
-    res.json({ ok: 1 });
+app.post('/ship', async (req, res, next) => {
+    try {
+        const { id, direction, x, y, ship_type } = req.body;
+        await gameService.placeShip(id, ship_type, x, y, direction);
+        res.json({ ok: 1 });
+    } catch (error) {
+        next(error);
+    }
 });
 
-app.use(logErrors);
-function logErrors(err, req, res, next) {
-    console.error(err.stack);
-    next(err);
-}
+app.use(function (error, req, res, next) {
+    res.status(400).json({ status: 400, message: error.message });
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
