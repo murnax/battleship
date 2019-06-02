5375d1bc76eddfea2bc16e2b1832324616d10dd7
Suratin Pattanawongthai <suratin.p@gobike.asia>
2019-06-02 17:07:17 +0700

    update reconstitution

--- src/Game/GameFactory.js
+++ src/Game/GameFactory.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const { Game } = require('.');
 const { Grid, GridType } = require('.').Grid;
+const { Ship } = require('.').Ship;
 
 class GameFactory {
 
@@ -19,15 +20,28 @@ class GameFactory {
      */
     reconstitute(gameEntityModel) {
         if (!gameEntityModel.deployedShips) gameEntityModel.deployedShips = {};
+        for (let deployedShip of Object.values(gameEntityModel.deployedShips)) {
+            const { id, type, isAttacked } = deployedShip.ship;
+            const ship = Ship.create(id, type, isAttacked);
+            deployedShip.ship = ship;
+            deployedShip.grids = deployedShip.grids.map(grid => {
+                const { type, x, y, isAttacked } = grid;
+                return Grid.create(type, x, y, isAttacked, { ship });
+            });
+            deployedShip.grids.forEach(grid => {
+                const { x, y } = grid;
+                gameEntityModel.board[y][x] = grid;
+            });
+        }
 
         gameEntityModel.board = gameEntityModel.board.map(n => n.map(grid => {
+            if (grid.type === GridType.SHIP) return grid;
             const { type, x, y, isAttacked, available } = grid;
             return Grid.create(type, x, y, isAttacked, { available });
         }));
 
         gameEntityModel = _.pick(gameEntityModel, Object.keys(new Game('1', 10, 10)));
         const game = Object.assign(new Game('1', 10, 10), gameEntityModel);
-
         return game;
     }
 }
